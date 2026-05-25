6eb6b27b729ae6356351c96ef8c8c3b7a9046406
Rei . <rei.@MacBookAir.lan>
2026-05-24 23:15:24 -0400

    fix: enable JSX in .js files

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   base: "./",
   plugins: [
-    react(),
+    react({ include: /\.(js|jsx|ts|tsx)$/ }),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
